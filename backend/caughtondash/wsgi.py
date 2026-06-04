512f51264e80f5a9b7fc1c4f339f8f9e1071b9a1
cadendengel <oaa54@txstate.edu>
2026-06-04 02:15:25 -0500

    Remove auto migration bug

--- backend/caughtondash/wsgi.py
+++ backend/caughtondash/wsgi.py
@@ -2,21 +2,9 @@
 
 import os
 
-from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 
-def _apply_pending_migrations() -> None:
-	if os.getenv('SKIP_AUTO_MIGRATE', '').lower() in {'1', 'true', 'yes'}:
-		return
-
-	if not os.getenv('DATABASE_URL'):
-		return
-
-	call_command('migrate', interactive=False, run_syncdb=True, verbosity=0)
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'caughtondash.settings')
-_apply_pending_migrations()
 
 application = get_wsgi_application()
